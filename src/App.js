3246076dd3117f5e14d19e2200d5a0d4410df2ec
임동민 <limdongmin@imdongmin-ui-MacBookAir.local>
2022-04-12 16:52:59 +0900

    Start sharp7

--- src/App.js
+++ src/App.js
@@ -1,20 +1,4 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react"; // useState를 사용하기 위한 import
-
 function App() {
-  const [SAH, setSAH] = useState("hide");
-  const SayHello = () => {
-    return <h1>Hello~!</h1>;
-  };
-  function onClick(event) {
-    event.target.innerText === "show" ? setSAH("hide") : setSAH("show");
-  }
-  return (
-    <>
-      {SAH === "hide" ? <SayHello /> : null}
-      <Button text={SAH} onClick={onClick} />
-    </>
-  );
+  return <></>;
 }
 export default App;
