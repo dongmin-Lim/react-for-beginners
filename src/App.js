5aa40370c0ad0295706476be492919090b59ef6d
임동민 <limdongmin@imdongmin-ui-MacBookAir.local>
2022-04-13 17:09:18 +0900

    초깃값

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react"; // useState를 사용하기 위한 import
 
 function App() {
-  const [SAH, setSAH] = useState("hide");
-  const SayHello = () => {
-    return <h1>Hello~!</h1>;
-  };
-  function onClick(event) {
-    event.target.innerText === "show" ? setSAH("hide") : setSAH("show");
-  }
-  return (
-    <>
-      {SAH === "hide" ? <SayHello /> : null}
-      <Button text={SAH} onClick={onClick} />
-    </>
-  );
+  return <></>;
 }
 export default App;
