e3e7a7cdba6b00affa6b1adca7175f938c81a6d6
임동민 <limdongmin@imdongmin-ui-MacBookAir.local>
2022-04-13 12:26:44 +0900

    #7.0 ~ #7.1 To-Do-List

--- src/App.js
+++ src/App.js
@@ -1,4 +1,41 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos([toDo, ...toDos]);
+    setToDo("");
+    console.log(toDos);
+  };
+  const removeToDo = (event) => {
+    const li = event.target.parentElement;
+    li.remove();
+  };
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" />
+        <button>click</button>
+        <ul>
+          {toDos.map((value, index) => (
+            <li key={index}>
+              {value}
+              <button onClick={removeToDo}>X</button>
+            </li>
+          ))}
+        </ul>
+      </form>
+    </>
+  );
 }
 export default App;
